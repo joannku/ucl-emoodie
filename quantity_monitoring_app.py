d5dde308ab67aaa8fe2a859ff4d63a3d671a4652
Joanna <joannakuc@ymail.com>
2022-11-29 09:45:21 +0000

    Remove slider, automated days passed counting integrated

--- quantity_monitoring_app.py
+++ quantity_monitoring_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.markdown("Credits calculator for eMoodie/UCL study")
 
 with st.form(key='form'):
-    days_passed = st.slider("How many days have passed since start of the study?", 1, 14, 1)
+    # days_passed = st.slider("How many days have passed since start of the study?", 1, 14, 1)
     uploaded_file = st.file_uploader("Upload excel file with participant results")
     submit_button = st.form_submit_button(label='Submit')
 
@@ -23,12 +23,17 @@ with st.form(key='form'):
             output_df = pd.DataFrame.from_dict(output).T.drop(columns=['######'])
             output_df.sort_index()
 
+            output_df.reindex(sorted(output_df.columns), axis=1)
+
             # Morning thought survey
             MTH = 40
             MTH_daily = MTH
             # Thought survey
             THO = 36
             THO_daily = THO*5
+
+            # Days passed
+            days_passed = len(output_df.columns)
 
             daily_total = MTH_daily + THO_daily
             daily_per_survey = daily_total / 6
@@ -38,10 +43,10 @@ with st.form(key='form'):
             survey_sum['surveys_filled'] = survey_sum.sum(axis=1)
             survey_sum['percentage_filled'] = survey_sum['surveys_filled'] / total_surveys_expected
 
-            survey_sum["Credits"] = [4 if value >= 0.7
-                                     else 3 if value >= 0.5
-                                     else 2 if value >= 0.2
-                                     else 1
+            survey_sum["Credits for surveys"] = [3 if value >= 0.7
+                                     else 2 if value >= 0.5
+                                     else 1 if value >= 0.2
+                                     else 0
                                      for value in survey_sum['percentage_filled']]
 
             st.dataframe(survey_sum)
